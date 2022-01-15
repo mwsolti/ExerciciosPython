0239d4d202b26bd54863cce52676869c39b4f3a4
mwsolti <85954308+mwsolti@users.noreply.github.com>
2022-01-15 16:12:41 -0300

    Add files via upload

---
+++ NovoSite/meuApp/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Usuario
+
+class UsuarioForm(forms.ModelForm):
+    class Meta:
+        model = Usuario
+        fields = ["nome", "email", "sexo","idade",'pokemon','outro_pokemon']
+
+
+        
